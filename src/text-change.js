00fb364313a61659dcff7cc03959e0b9c0a6e62b
Edward Deam <edwardcrdeam@gmail.com>
2019-03-21 16:17:11 +1100

    Added javascript onclick functions for skill section

---
+++ src/text-change.js
@@ -0,0 +1,12 @@
+function displayHTML() {
+  document.getElementById("skill-title").innerHTML = "HTML"
+  document.getElementById("skill-text").innerHTML = "Information about my skills in HTML"
+}
+function displayCSS() {
+  document.getElementById("skill-title").innerHTML = "CSS"
+  document.getElementById("skill-text").innerHTML = "Information about my skills in CSS"
+}
+function displayRuby() {
+  document.getElementById("skill-title").innerHTML = "Ruby"
+  document.getElementById("skill-text").innerHTML = "Information about my skills in Ruby"
+}
